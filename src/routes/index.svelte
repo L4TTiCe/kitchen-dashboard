621afe19f8b367d02132c1e08de58a834710ddba
L4TTiCe <dheshan.mail@gmail.com>
2022-04-23 21:12:43 -0400

    echo server URL to console

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+    import { variables } from '$lib/env';
+    console.log("Using Server: " + variables.SERVER_URL)
+</script>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
